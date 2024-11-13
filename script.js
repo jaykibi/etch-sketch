fceed4697d3b2358804941a0ba880cd686c7e845
kibi <raquibbi@gmail.com>
2024-11-12 19:09:43 -0600

    init commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// need to create 16x16 grid, use flexbox
+// dont hard code 16x16 grid, use js to create divs
+
+// grab container from html
+const container = document.querySelector("#container");
